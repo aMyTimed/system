eec0bf4978cf08e7d983cb1d85b688f2dd8f83dc
asour <52638772+asour8@users.noreply.github.com>
2024-01-18 15:03:19 -0400

    e

--- configuration.nix
+++ configuration.nix
@@ -141,4 +141,10 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
+  hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932 
+     enable = true; 
+     driSupport = true; 
+     driSupport32Bit = true; 
+   }; 
+
 }
