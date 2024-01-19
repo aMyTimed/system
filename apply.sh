ed7f5cc7fad6cf88e34d469d2fd480bf41d0fb2f
asour <52638772+asour8@users.noreply.github.com>
2024-01-19 14:46:12 -0400

    new apply.sh

--- apply.sh
+++ apply.sh
@@ -1,2 +1,14 @@
-sudo cp ./configuration.nix /etc/nixos/configuration.nix
+# purge all files in /etc/nixos so no lingering files are left
+sudo rm -rf /etc/nixos/*
+sudo cp -r ./src/ /etc/nixos/
+echo "Copied files to /etc/nixos/";
+
+# Each time, we generate a fresh hardware configuration.
+# This is useful because it ensures we never break the hardware config or accidentally delete it,
+# it will ensure our system is fully deterministic since it makes this file from scratch each time. 
+nixos-generate-config --show-hardware-config | sudo dd status=none of=/etc/nixos/hardware-configuration.nix
+echo "Generated hardware configuration";
+
 sudo nixos-rebuild switch
+
+echo "\nAll done!";
