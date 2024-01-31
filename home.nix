69d30098e673d6f40d5d0453125072e6e57417ce
asour <52638772+asour8@users.noreply.github.com>
2024-01-31 15:45:22 -0400

    codium alias

--- home.nix
+++ home.nix
@@ -102,6 +102,7 @@
     shellAliases = {
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      code = "codium $@"; # muscle memory
     };
   };
 
