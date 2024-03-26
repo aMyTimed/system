09876215981c72767d1f0ff38d3a87436aa60efd
asour <52638772+asour8@users.noreply.github.com>
2024-03-26 18:12:05 -0300

    E

--- hosts/LENOVO720/configuration.nix
+++ hosts/LENOVO720/configuration.nix
@@ -12,7 +12,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.configurationLimit = 1;
+  boot.loader.configurationLimit = 1;
 
   networking.hostName = "LENOVO720";
 
