cbc36b67e1d60493c1fb478771e5b1154c0010b7
asour <52638772+asour8@users.noreply.github.com>
2024-03-26 18:09:14 -0300

    E

--- hosts/LENOVO720/configuration.nix
+++ hosts/LENOVO720/configuration.nix
@@ -6,9 +6,12 @@
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "/dev/nvme0n1";
+  #boot.loader.grub.useOSProber = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "LENOVO720";
 
