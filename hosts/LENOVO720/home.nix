5868ae816e97e4e00800f97ccfb2feaa7ada773e
asour <52638772+asour8@users.noreply.github.com>
2024-03-26 19:14:14 -0300

    untested_up

--- hosts/LENOVO720/home.nix
+++ hosts/LENOVO720/home.nix
@@ -5,4 +5,12 @@
     [
       ../../home/home.nix
     ];
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1, 1920x1080, 0x0, 1"
+      "DP-2, 1920x1080, 1920x0, 1"
+      "DP-3, 1920x1080, 3840x0, 1"
+    ]
+  };
 }
